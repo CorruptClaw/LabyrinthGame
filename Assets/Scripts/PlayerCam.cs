26280d9b230e235f14a3450030a1af82c1dcbe5c
CorruptClaw <sebastianhaag4@gmail.com>
2025-05-19 15:55:29 +0200

    Test1

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -5,24 +5,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerCam : MonoBehaviour
 {
-    public Transform Body;
-
-    [SerializeField]
-    private float _cameraSensetivity;
-
-    private float _pitch;
-
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Body.Rotate(0f, Input.GetAxis("Mouse X") * _cameraSensetivity * Time.deltaTime, 0f);
-        GetComponent<Transform>().Rotate(Input.GetAxis("Mouse Y") * -_cameraSensetivity * Time.deltaTime, 0f, 0f);
+       
     }
+
+
 }
--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementScript : MonoBehaviour
 {
+    public float speed;
+
+    private Rigidbody _playerRigidbody;
+    private float movementX;
+    private float movementY;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
